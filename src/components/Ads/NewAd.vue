d76f79d4f3361b7c96444e6b168daa9b8d0bf1ee
Elizarov Andrei <elizarov13@yandex.ru>
2019-03-28 23:37:50 +0300

    третий коммит

--- src/components/Ads/NewAd.vue
+++ src/components/Ads/NewAd.vue
@@ -1,8 +1,49 @@
 <template>
   <v-container>
     <v-layout row>
-      <v-flex>
-        <h1>NewAd</h1>
+      <v-flex xs12 sm6 offset-sm3>
+        <h1 class="text--secondary mb-3">create new ad</h1>
+        <v-form v-model="valid" ref="form" validation mb-3>
+          <v-text-field
+            name="title"
+            label="ad title"
+            type="text"
+            v-model="title"
+            required
+            :rules="[v => !!v || 'title is required']"
+          ></v-text-field>
+          <v-textarea
+            name="description"
+            label="Ad description"
+            type="text"
+            v-model="description"
+            :rules="[v => !!v || 'description is required']"
+          ></v-textarea>
+        </v-form>
+        <v-layout row mb-3>
+          <v-flex xs12>
+            <v-btn class="warning">
+              загрузить
+              <v-icon right dark>cloud_upload</v-icon>
+            </v-btn>
+          </v-flex>
+        </v-layout>
+        <v-layout row>
+          <v-flex xs12>
+            <img src="https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg" alt height="100">
+          </v-flex>
+        </v-layout>
+        <v-layout row>
+          <v-flex xs12>
+            <v-switch label="add to promo" v-model="promo"></v-switch>
+          </v-flex>
+        </v-layout>
+        <v-layout row>
+          <v-flex xs12>
+            <v-spacer></v-spacer>
+            <v-btn :disabled="!valid" class="succes" @click="createAd">Create ad</v-btn>
+          </v-flex>
+        </v-layout>
       </v-flex>
     </v-layout>
   </v-container>
@@ -11,7 +52,24 @@
 <script>
 export default {
   data () {
-    return {}
+    return {
+      title: '',
+      description: '',
+      promo: false,
+      valid: false
+    }
+  },
+  methods: {
+    createAd () {
+      if (this.$refs.form.validate()) {
+        const ad = {
+          title: this.title,
+          description: this.description,
+          promo: this.promo
+        }
+        console.log(ad)
+      }
+    }
   }
 }
 </script>
--- src/components/User/Orders.vue
+++ src/components/User/Orders.vue
@@ -1,8 +1,27 @@
 <template>
   <v-container>
     <v-layout row>
-      <v-flex>
-        <h1>orders</h1>
+      <v-flex xs12 sm6 offset-sm3>
+        <h1 class="text--secondary mb-3">Orders</h1>
+
+        <v-list subheader two-line>
+          <v-list-tile avatar v-for="order in orders" :key="order.id">
+            <v-list-tile-action>
+              <v-checkbox
+              :input-value="order.done"
+              @click="markDone(order)"></v-checkbox>
+            </v-list-tile-action>
+
+            <v-list-tile-content>
+              <v-list-tile-title>{{order.name}}</v-list-tile-title>
+              <v-list-tile-sub-title>{{order.phone}}</v-list-tile-sub-title>
+            </v-list-tile-content>
+            <v-list-tile-action>
+              <v-btn
+              :to="'/ad/' + order.id" class="primary">Open</v-btn>
+            </v-list-tile-action>
+          </v-list-tile>
+        </v-list>
       </v-flex>
     </v-layout>
   </v-container>
@@ -11,7 +30,22 @@
 <script>
 export default {
   data () {
-    return {}
+    return {
+      orders: [
+        {
+          id: 'Efds6',
+          name: 'vlad',
+          phone: '8-800-555',
+          adId: '23',
+          done: false
+        }
+      ]
+    }
+  },
+  methods: {
+    markDone (order) {
+      order.done = true
+    }
   }
 }
 </script>
